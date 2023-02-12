355622323b51d8b95f827c88937f5c271bdcdb23
kohj1018 <kohj1018@hanyang.ac.kr>
2023-02-13 03:06:18 +0900

    first commit

---
+++ src/main/java/kr/somapeople/somapeopleback/SomaPeopleBackApplication.java
@@ -0,0 +1,13 @@
+package kr.somapeople.somapeopleback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SomaPeopleBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SomaPeopleBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/somapeople/somapeopleback/SomaPeopleBackApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.somapeople.somapeopleback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SomaPeopleBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
