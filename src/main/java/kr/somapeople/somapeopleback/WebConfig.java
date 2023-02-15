fa2fd1acc75acf9221b689e9a0ecf565eee2658a
kohj1018 <kohj1018@hanyang.ac.kr>
2023-02-16 03:58:56 +0900

    CORS 설정

--- src/main/java/kr/somapeople/somapeopleback/WebConfig.java
+++ src/main/java/kr/somapeople/somapeopleback/WebConfig.java
@@ -9,9 +9,17 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "http://localhost:8080")
-                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
-                .allowCredentials(true)
-                .maxAge(3000);
+                .allowedOrigins(
+                        "https://www.somapeople.kr",
+                        "https://somapeople.kr",
+                        "https://www.somapeople.xyz",
+                        "https://somapeople.xyz",
+                        "http://localhost:3000",
+                        "http://localhost:8080",
+                        "https://soma-people-web.vercel.app"
+                )    // CORS 허용할 출처
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")    // 허용할 HTTP method
+                .allowCredentials(true) // 쿠키 인증 요청 허용
+                .maxAge(3000);  // 원하는 시간만큼 pre-flight 리퀘스트를 캐싱
     }
 }
