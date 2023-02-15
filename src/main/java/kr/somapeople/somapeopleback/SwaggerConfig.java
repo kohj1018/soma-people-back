c22e17fb4e87df1eababd4e8a53094171a9c94ae
kohj1018 <kohj1018@hanyang.ac.kr>
2023-02-16 04:25:43 +0900

    swagger server url 수정

--- src/main/java/kr/somapeople/somapeopleback/SwaggerConfig.java
+++ src/main/java/kr/somapeople/somapeopleback/SwaggerConfig.java
@@ -1,11 +1,14 @@
 package kr.somapeople.somapeopleback;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@OpenAPIDefinition(servers = {@Server(url = "https://somapeople.xyz", description = "SomaPeople 공식 서버 URL")})
 @Configuration
 public class SwaggerConfig {
 
@@ -23,6 +26,6 @@ public class SwaggerConfig {
                 .info(new Info()
                         .title("SomaPeople API")
                         .description("소마인(SomaPeople) 프로젝트 API 명세서입니다.")
-                        .version("v0.1.0"));
+                        .version("v0.1.1"));
     }
 }
